0e9bd3ecb7895b035fc5973c640c6ac168d86d02
sithanh1602 <164637916+sithanh1602@users.noreply.github.com>
2024-07-01 15:10:31 +0700

    thanhComit

---
+++ src/main/java/main/DU_AN_1_GR1_QLBACHHOA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author NITRO 5
+ */
+public class DU_AN_1_GR1_QLBACHHOA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
